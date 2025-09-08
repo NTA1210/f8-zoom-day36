937fcb1e0cab5e907663673694718d8746661b3f
NTA1210 <anhntde180721@fpt.edu.vn>
2025-09-08 19:46:43 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/f8-zoom-day35/", // Tên repo của bạn
+  base: "/f8-zoom-day36/", // Tên repo của bạn
 });
